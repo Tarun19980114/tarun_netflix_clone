da7d5d1a10d7861be81afb59385d390f20e9564b
Tarun <tarundjrock44@gmail.com>
2022-10-03 13:24:24 +0530

    second

---
+++ index.js
@@ -0,0 +1,16 @@
+// Light & Dark Mode
+function changemode(){
+    let mybody = document.body;
+    mybody.classList.toggle('mydark')
+}
+
+// Coupon
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('banner-bar').style.opacity = '.7';
+}
+
+function closecoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('banner-bar').style.opacity = '1';
+}
